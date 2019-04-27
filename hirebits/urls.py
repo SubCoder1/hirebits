09e4efec1099a2c140ed8604bf061c1988728d19
SubCoder1 <ghoshsoumakanti@gmail.com>
2019-04-28 03:30:44 +0530

    added problems page path in urls.py

--- hirebits/urls.py
+++ hirebits/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from dashboard.views import landing_page_view
 from AUth.views import login_view, register_view, reset_view, forgot_pass_view
+from dashboard.views import problem_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,4 +11,5 @@ urlpatterns = [
     path('register/', register_view, name='register_view'),
     path('fg-pass/', forgot_pass_view, name='forgot_pass'),
     path('rs-pass/', reset_view, name='reset_view'),
+    path('problems/', problem_page, name='prob_page'),
 ]
