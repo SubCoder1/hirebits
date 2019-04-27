5c391e941c705b8fdb92784d915e135cdbeae3bd
SubCoder1 <ghoshsoumakanti@gmail.com>
2019-04-27 22:37:35 +0530

    connected landing page and AUth pages with backend

--- AUth/views.py
+++ AUth/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def login_view(request):
+    return render(request, 'login.html', {})
+
+def register_view(request):
+    return render(request, 'register.html', {})
+
+def reset_view(request):
+    return render(request, 'reset.html', {})
+
+def forgot_pass_view(request):
+    return render(request, 'forgot.html', {})
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
 # Create your views here.
+
+class landing_page_view(TemplateView):
+    template_name = "index.html"
