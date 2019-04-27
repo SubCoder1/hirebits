08ff60a77225b69da612e1773f5aa0d59e34ca17
SubCoder1 <ghoshsoumakanti@gmail.com>
2019-04-28 03:31:55 +0530

    added problem_statements model in models.py

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Problem_statements(models.Model):
+    s_name = models.CharField(max_length=200)
+    s_descrip = models.CharField(max_length=400)
+    s_content = models.TextField()
+    s_company = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.s_name
